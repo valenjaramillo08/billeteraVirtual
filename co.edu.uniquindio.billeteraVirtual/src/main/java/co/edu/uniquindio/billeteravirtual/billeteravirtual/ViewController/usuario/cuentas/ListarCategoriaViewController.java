09328f55770ce82c268aca5c6d7380c0c0e20846
laurabareno716 <lauram.barenob@uqvirtual.edu.co>
2025-05-10 10:47:36 -0500

    cuentas

---
+++ co.edu.uniquindio.billeteraVirtual/src/main/java/co/edu/uniquindio/billeteravirtual/billeteravirtual/ViewController/usuario/cuentas/ListarCategoriaViewController.java
@@ -0,0 +1,33 @@
+package co.edu.uniquindio.billeteravirtual.billeteravirtual.ViewController.usuario.cuentas;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
+public class ListarCategoriaViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<?> cbCategoria;
+
+    @FXML
+    private TextArea taCategorias;
+
+    @FXML
+    private Button btConsultar;
+
+    @FXML
+    void initialize() {
+        
+
+    }
+}
+
