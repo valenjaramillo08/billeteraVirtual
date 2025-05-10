66286e4ba51e3364a810f8d26c57d432629b1305
Valentina Orozco J. <valenjaramillo08@gmail.com>
2025-05-10 17:15:32 -0500

    adm

---
+++ co.edu.uniquindio.billeteraVirtual/src/main/java/co/edu/uniquindio/billeteravirtual/billeteravirtual/ViewController/GestionTransaccionesViewController.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.billeteravirtual.billeteravirtual.ViewController;
+
+import javafx.event.ActionEvent;
+
+public class GestionTransaccionesViewController {
+    public void onCrearTransaccion(ActionEvent event) {
+    }
+}
