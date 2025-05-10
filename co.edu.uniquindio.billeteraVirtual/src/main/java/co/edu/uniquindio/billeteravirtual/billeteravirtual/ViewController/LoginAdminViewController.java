ba0eb8c5fd12085f413a309769d75d7515d635dc
Valentina Orozco J. <valenjaramillo08@gmail.com>
2025-05-10 10:32:57 -0500

    patron proxy
    proxy

--- co.edu.uniquindio.billeteraVirtual/src/main/java/co/edu/uniquindio/billeteravirtual/billeteravirtual/ViewController/LoginAdminViewController.java
+++ co.edu.uniquindio.billeteraVirtual/src/main/java/co/edu/uniquindio/billeteravirtual/billeteravirtual/ViewController/LoginAdminViewController.java
@@ -5,11 +5,13 @@ import co.edu.uniquindio.billeteravirtual.billeteravirtual.Controller.LoginUsuar
 import co.edu.uniquindio.billeteravirtual.billeteravirtual.HelloApplication;
 import co.edu.uniquindio.billeteravirtual.billeteravirtual.Model.Administrador;
 import co.edu.uniquindio.billeteravirtual.billeteravirtual.Model.Usuario;
+import co.edu.uniquindio.billeteravirtual.billeteravirtual.Proxy.AutenticacionProxy;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -23,6 +25,7 @@ public class LoginAdminViewController {
 
     LoginAdminController loginAdminController;
     HelloApplication helloApp;
+    private AutenticacionProxy autenticacion;
 
 
     public void setApp(HelloApplication helloApplication) {
@@ -61,6 +64,7 @@ public class LoginAdminViewController {
     @FXML
     void initialize() {
         loginAdminController = new LoginAdminController();
+        autenticacion = new AutenticacionProxy(loginAdminController);
     }
 
     // private void validarCredenciales(ActionEvent event) throws IOException {
@@ -74,9 +78,10 @@ public class LoginAdminViewController {
         String id = txt_id_admin.getText();
         String clave = txt_clave.getText();
 
-        Administrador valid = loginAdminController.loginAdmin(id, clave);
+        boolean validado=autenticacion.autenticar(id, clave);
 
-        if (valid != null) {
+        if (validado) {
+            Administrador admin = loginAdminController.loginAdmin(id, clave);
             //Usuario usuarioLogueado = loginUsuarioController.obtenerUsuario(id);
 
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/billeteravirtual/billeteravirtual/administradorVentanaPrincipal.fxml"));
@@ -84,15 +89,37 @@ public class LoginAdminViewController {
 
             AdministradorVentanaPrincipalViewController controller = loader.getController();
             controller.setApp(helloApp);
-            controller.setAdministradorLogueado(valid); // este método se encarga de pasar el usuario a las pestañas
+            controller.setAdministradorLogueado(admin); // este método se encarga de pasar el usuario a las pestañas
 
             Stage stage = new Stage();
             stage.setScene(new Scene(root));
             stage.show();
 
             ((Stage) ((Button) event.getSource()).getScene().getWindow()).close();
+
+            mostrarAlertaInfo("Bienvenido Administrador" + admin.getNombre());
         } else {
-            System.out.println("Credenciales inválidas.");
+            if (autenticacion.estaBloqueado()) {
+                mostrarAlertaError("Has excedido los 3 intentos. El acceso ha sido bloqueado.");
+            } else {
+                mostrarAlertaError("Credenciales incorrectas. Intenta nuevamente.");
+            }
         }
+    }
+
+    private void mostrarAlertaError(String mensaje) {
+        Alert alerta = new Alert(Alert.AlertType.ERROR);
+        alerta.setTitle("Error de Autenticación");
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
+    }
+
+    private void mostrarAlertaInfo(String mensaje) {
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.setTitle("Inicio de sesión");
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
     }
 }
